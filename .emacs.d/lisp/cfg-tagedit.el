4068bce3112977a1c1de96a411710adf32fa53c6
Auclair Emmanuel <auclair.emmanuel.prestataire@sfr.com>
2017-02-07 18:42:38 +0100

    Emacs : disable paredit binding in tagedit

--- .emacs.d/lisp/cfg-tagedit.el
+++ .emacs.d/lisp/cfg-tagedit.el
@@ -2,5 +2,6 @@
   :ensure t
   :init
   (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))
-  :config
-  (tagedit-add-paredit-like-keybindings))
+  ;; :config
+  ;; (tagedit-add-paredit-like-keybindings)
+	)
