82abfd1858031bf5f0e8d4b3a2e8474c03d53147
Auclair Emmanuel <auclair.emmanuel.prestataire@sfr.com>
2017-07-26 10:06:08 +0200

    Emacs: org cheatsheet update

--- .emacs.d/lisp/cfg-org.el
+++ .emacs.d/lisp/cfg-org.el
@@ -26,17 +26,31 @@
      (css . t)
      (plantuml . t)))
   (cheatsheet-add-group 'Org
-                        '(:key "<s-tab" :description "Insert source code block")
-                        '(:key "M-left/right" :description "Decrease/Increase header level")
+
+												'(:key "M-left/right" :description "Decrease/Increase header level")
 												'(:key "C-c C-u" :description "Back to top level header")
 												'(:key "C-c C-n" :description "Next header (any level)")
 												'(:key "C-c C-p" :description "Previous header (any level)")
 												'(:key "C-c C-f" :description "Next header (same level)")
 												'(:key "C-c C-b" :description "Previous header (same level)")
+
+												'(:key "M-<RET>" :description "List: insert new item at same level")
+												'(:key "S-<up>" :description "List: previous item")
+												'(:key "S-<down>" :description "List: next item")
+												'(:key "S-<left/right>" :description "List: cycle bullet type")
+												'(:key "M-<up>" :description "List: move item up")
+												'(:key "M-<down>" :description "List: move item down")
+												'(:key "M-<left>" :description "List: decrease item indentation")
+												'(:key "M-<right>" :description "List: increase item indentation")
+												'(:key "M-S-<left>" :description "List: decrease item+children indentation")
+												'(:key "M-S-<right>" :description "List: increase item+children indentation")
+
+                        '(:key "<s-tab" :description "Insert source code block")
 												'(:key "C-c '" :description "Edit code block")
                         '(:key "C-c C-c" :description "Evaluate code block")
                         '(:key "C-c C-e" :description "Export file")
                         '(:key "C-c C-v t" :description "Tangle file")
+
                         '(:key "C-c C-x C-v" :description "Toggle inline images")
                         '(:key "C-c C-x C-M-v" :description "Refresh inline images")))
 ;; (use-package org-plus-contrib
