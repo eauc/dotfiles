d1f3ee7183cc369157b1fe14bc6329efa30ff368
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2023-04-18 00:11:34 +0200

    Gitconfig: org -> neorg

--- nvim/lua/user/plugins/neorg.lua
+++ nvim/lua/user/plugins/neorg.lua
@@ -1,6 +1,7 @@
 require('neorg').setup({
   load = {
     ["core.defaults"] = {},       -- Loads default behaviour
+    ["core.export"] = {}, -- Adds pretty icons to your documents
     ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
     ["core.norg.completion"] = {
       config = {
