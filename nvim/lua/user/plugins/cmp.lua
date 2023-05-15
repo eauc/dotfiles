40c6ec3fd6e58cd68578fa0afb067c6446c62220
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2023-05-16 00:39:52 +0200

    Nvim tweak completion

--- nvim/lua/user/plugins/cmp.lua
+++ nvim/lua/user/plugins/cmp.lua
@@ -2,17 +2,17 @@ local cmp = require('cmp')
 local luasnip = require('luasnip')
 local lspkind = require('lspkind')
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-end
+-- local has_words_before = function()
+--   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+-- end
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,longest,preview'
 
 cmp.setup({
   experimental = {
-    ghost_text = true,
+    ghost_text = false,
   },
   formatting = {
     format = lspkind.cmp_format(),
@@ -23,18 +23,25 @@ cmp.setup({
     end,
   },
   mapping = {
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(1) then
-        luasnip.jump(1)
-      elseif cmp.visible() then
-        cmp.select_next_item()
-      elseif has_words_before() then
-        cmp.complete()
+    ['<CR>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.confirm()
       else
         fallback()
       end
     end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    ['<A-right>'] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(1) then
+        luasnip.jump(1)
+      elseif cmp.visible() then
+        cmp.select_next_item()
+      -- elseif has_words_before() then
+      --   cmp.complete()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<A-left>'] = cmp.mapping(function(fallback)
       if luasnip.jumpable(-1) then
         luasnip.jump(-1)
       elseif cmp.visible() then
@@ -47,10 +54,10 @@ cmp.setup({
   sources = {
     { name = 'nvim_lsp' },
     { name = 'nvim_lsp_signature_help' },
+    { name = 'conjure' },
+    { name = 'neorg' },
+    { name = 'buffer' },
     { name = 'luasnip' },
     { name = 'path' },
-    { name = 'buffer' },
-    { name = "neorg" },
-    { name = "conjure" },
   },
 })
