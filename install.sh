36aad393b2b5474c7b05babd92643292be84d309
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2025-03-21 20:32:09 +0100

    Install: tree + nerd fonts + nvim

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ echo "Installing apt packages"
 sudo apt install -y \
   curl \
   git \
+  tree \
   build-essential \
   autotools-dev dh-autoreconf \
   fzf \
@@ -13,6 +14,35 @@ sudo apt install -y \
 if ! command -v -- "kitty" > /dev/null 2>&1; then
   echo "Installing kitty"
   curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+  # Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
+  # your system-wide PATH)
+  ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+  # Place the kitty.desktop file somewhere it can be found by the OS
+  cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+  # Update the paths to the kitty and its icon in the kitty desktop file(s)
+  sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+  sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+  # Make xdg-terminal-exec (and hence desktop environments that support it use kitty)
+  echo 'kitty.desktop' > ~/.config/xdg-terminals.list
+fi
+
+
+if [ ! -f ~/.local/share/fonts/JetBrainsMono.zip ]; then
+  echo "Installing Nerd fonts"
+  mkdir -p ~/.local/share/fonts
+  cd ~/.local/share/fonts
+  curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
+  unzip JetBrainsMono.zip
+  cd -
+fi
+
+if ! command -v -- "nvim" > /dev/null 2>&1; then
+  echo "Installing nvim"
+  cd ~/Downloads
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+  sudo rm -rf /opt/nvim
+  sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+  cd -
 fi
 
 echo "Installing mise"
